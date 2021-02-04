6608ca2c1784f9368096d78107c25ff11b88f59b
Matt Ravenmoore <m.ravenmoore@gmail.com>
2021-02-03 20:31:18 -0800

    basics up

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import Nav from '../Nav/nav'
 
+const headerStyles = {
+  backgroundColor: 'yellow',
+}
+
+
 export default function Header(){
   return(
-    <div id="header">
+    <div id="header" style={headerStyles}>
       <h1>Matt Ravenmoore</h1>
       <p>Personal Portfolio</p>
       <Nav />
