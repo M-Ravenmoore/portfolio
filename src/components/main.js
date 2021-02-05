1583b1cac168dd5be5ae7eca78dafb9335eca919
Matt Ravenmoore <m.ravenmoore@gmail.com>
2021-02-04 23:16:08 -0800

    content spellchecked

--- src/components/main.js
+++ src/components/main.js
@@ -27,7 +27,7 @@ class Main extends React.Component {
           <h2 className="major">My Background</h2>
           <span className="image main">
           </span>
-          <p>Welcome to Matt Ravenmoore's technical resume, if you are looking for Matt as a coder, developer or enginer you have gotten to the right portfolio. If you are looking to see his other portfolio for metal fabrication and art click the button below to be taken to Ravenmoore Valley his family crafting business. </p>
+          <p>Welcome to Matt Ravenmoore's technical resume, if you are looking for Matt as a coder, developer or engineer, you have gotten to the right portfolio. If you are looking to see his other portfolio for metal fabrication and art, click the button below to be taken to Ravenmoore Valley, his family crafting business. </p>
 
           <a href='https://ravenmoorevalley.ucraft.site/'><button>Ravenmoore Valley</button></a>
 
@@ -42,7 +42,7 @@ class Main extends React.Component {
           style={{ display: 'none' }}
         >
           <h2 className="major">My Work</h2>
-          <p>For the last 2 years i have been working on building technical projects and applications. I have chosen a few to showcase the skills I have achived and get my work out into the world. These projects range from simple express servers to full stack applications. there even is a mobile app aimed for android in there. clicke the button below to see what I have cooked up</p>
+          <p>For the last 2 years I have been working on building technical projects and applications. I have chosen a few to showcase the skills I have achieved and to get my work out into the world. These projects range from simple express servers to full stack applications. There is even a mobile app aimed for Android in there. Click the button below to see what I have cooked up.</p>
 
           <a href='/projects'><button>Projects</button></a>
           {close}
@@ -58,7 +58,7 @@ class Main extends React.Component {
           <span className="Headshot">
             <img  className='Headshot' src={Headshot} alt="Matt Ravenmoore Headshot"/>
           </span>
-          <p>I am Matt Ravenmoore, and I am a software enginer. My backgound is in creating magic, of variying kinds. From theater and circus magic of building and creating sets and performances coming together. To metal fabrication and blacksmithing, i have lead a life full of adventures and learning and strive to keep it that way. I push my self to try or learn 3-5 new things every day, this goal helps me push myself to be the best human i can.</p>
+          <p>I am Matt Ravenmoore and I am a software engineer. My backgound is in creating magic of varying kinds. From the theater and circus magics of building and creating sets and performances coming together. To metal fabrication and blacksmithing, I have lead a life full of adventures and learning and strive to keep it that way. I push my self to try or learn 3-5 new things every day, this goal helps me push myself to be the best human I can.</p>
           {close}
         </article>
 
